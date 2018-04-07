bd18f10d99f560a201557254b3e80280f16c3b40
Jiechao Wang <jiechao@starmobileinc.com>
2018-04-07 03:01:11 -0400

    POJOs finally work

--- src/main/java/com/wjc/api/TestPathHandler.java
+++ src/main/java/com/wjc/api/TestPathHandler.java
@@ -1,16 +1,19 @@
-package com.wjc.api.archive;
+package com.wjc.api;
 
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.wjc.model.Request;
 import com.wjc.model.Response;
 import com.amazonaws.services.lambda.runtime.Context; 
 
-public class TestPath implements RequestHandler<Request, Response> {
+/**
+ * API gateway + lambda function + POJOs demo
+ * @author jiechao
+ */
+public class TestPathHandler implements RequestHandler<Request, Response> {
     
-	// TODO: Need to figure out if we can use handleRequest with API gateway
 	@Override
     public Response handleRequest(Request request, Context context) {
-        String greetingString = "Hello" + (request.id + request.time);
+        String greetingString = "Hello" + (request.getId() + request.getTime());
         return new Response(true, 2, greetingString);
     }
 }
--- src/main/java/com/wjc/model/Response.java
+++ src/main/java/com/wjc/model/Response.java
@@ -2,13 +2,8 @@ package com.wjc.model;
 
 public class Response {
 	
-	@SuppressWarnings("unused")
 	private boolean success;
-	
-	@SuppressWarnings("unused")
 	private int id;
-	
-	@SuppressWarnings("unused")
 	private String body;
 	
 	public Response() {}
@@ -16,6 +11,30 @@ public class Response {
 	public Response(boolean success, int id, String body) {
 		this.success = success;
 		this.id = id;
+		this.body = body;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
 		this.body = body;
 	}
 	
