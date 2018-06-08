6747a5dafef7f4435cf3844e54f190b637ef804c
Jiechao Wang <jiechao@starmobileinc.com>
2018-06-07 23:06:22 -0400

    Rental list

--- src/main/java/com/wjc/handler/RentalListHandler.java
+++ src/main/java/com/wjc/handler/RentalListHandler.java
@@ -20,7 +20,6 @@ public class RentalListHandler implements RequestHandler<RentalListRequest, Rent
 
 	@Override
 	public RentalListResponse handleRequest(RentalListRequest request, Context context) {
-		String description = "This is a very long long long long long long long long description from user in " + request.getCity();
 		List<String> imageUrls = new ArrayList<>();
 		imageUrls.add("https://room-matehotels.com/images/img/general/slide_inicio/slide_01.jpg");
 		imageUrls.add(
@@ -28,17 +27,34 @@ public class RentalListHandler implements RequestHandler<RentalListRequest, Rent
 		imageUrls.add("https://room-matehotels.com/images/img/general/slide_inicio/slide_01.jpg");
 		imageUrls.add(
 				"http://4.bp.blogspot.com/-RSAdi3NMMs8/VakWj_znRcI/AAAAAAAAAMI/lp19iktRyCw/s1600/Rent%2Broom%2Bstockholm.jpg");
-
-		RentalListCard card1 = new RentalListCard("$110/日", "布鲁克林下城", "Best apartment in Downtown Brooklyn", description + " and lets make it even longer longer longer", imageUrls);
-		RentalListCard card2 = new RentalListCard("$3110/月", "中城西", "Best apartment in Downtown Brooklyn", description, imageUrls);
-		RentalListCard card3 = new RentalListCard("$11022/年", "地狱厨房/克林顿区", "LIVE HERE!", description, imageUrls);
+		
+		RentalListCard card1 = new RentalListCard.Builder()
+				.id("1527978347|999")
+				.price(300000)
+				.timeUnit(1)
+				.title1("布鲁克林")
+				.title2("Best 公寓 in town")
+				.description("精装修，带家具，独立厨卫，包水电暖网络；交通方便，生活便利，房子干净整洁。")
+				.imageUrls(imageUrls)
+				.build();
+		
+		RentalListCard card2 = new RentalListCard.Builder()
+				.id("1517978300|12")
+				.price(450000)
+				.timeUnit(1)
+				.title1("Midtown")
+				.title2("位于皇冠高度区的精装公寓")
+				.description("精装修，带家具，独立厨卫，包水电暖网络；交通方便，生活便利，房子干净整洁。longer longer")
+				.imageUrls(imageUrls)
+				.build();
 		
 		RentalListData data = new RentalListData();
 		data.setTotalCount(100);
-		data.setResults(Arrays.asList(card1, card2, card3));
+		data.setResults(Arrays.asList(card1, card2));
 		RentalListResponse response = new RentalListResponse();
 		response.setStatusCode(200);
 		response.setData(data);
+		response.setMessage("a message");
 		return response;
 	}
 
--- src/main/java/com/wjc/model/RentalListCard.java
+++ src/main/java/com/wjc/model/RentalListCard.java
@@ -3,53 +3,129 @@ package com.wjc.model;
 import java.util.List;
 
 public class RentalListCard {
-	
-	private String price;
+
+	private String id;
+	// In US cents
+	private int price;
+	// For example, per month
+	private int timeUnit;
 	private String title1;
 	private String title2;
 	private String description;
 	private List<String> imageUrls;
-	
-	public RentalListCard() {}
-	
-	public RentalListCard(String price, String title1, String title2, String description, List<String> imageUrls) {
-		super();
-		this.price = price;
-		this.title1 = title1;
-		this.title2 = title2;
-		this.description = description;
-		this.imageUrls = imageUrls;
+
+	public String getId() {
+		return id;
 	}
-	
-	public String getPrice() {
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getPrice() {
 		return price;
 	}
-	public void setPrice(String price) {
+
+	public void setPrice(int price) {
 		this.price = price;
 	}
+
+	public int getTimeUnit() {
+		return timeUnit;
+	}
+
+	public void setTimeUnit(int timeUnit) {
+		this.timeUnit = timeUnit;
+	}
+
 	public String getTitle1() {
 		return title1;
 	}
+
 	public void setTitle1(String title1) {
 		this.title1 = title1;
 	}
+
 	public String getTitle2() {
 		return title2;
 	}
+
 	public void setTitle2(String title2) {
 		this.title2 = title2;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public List<String> getImageUrls() {
 		return imageUrls;
 	}
+
 	public void setImageUrls(List<String> imageUrls) {
 		this.imageUrls = imageUrls;
 	}
 
+	public static class Builder {
+		private String id;
+		private int price;
+		private int timeUnit;
+		private String title1;
+		private String title2;
+		private String description;
+		private List<String> imageUrls;
+
+		public Builder id(String id) {
+			this.id = id;
+			return this;
+		}
+
+		public Builder price(int price) {
+			this.price = price;
+			return this;
+		}
+
+		public Builder timeUnit(int timeUnit) {
+			this.timeUnit = timeUnit;
+			return this;
+		}
+
+		public Builder title1(String title1) {
+			this.title1 = title1;
+			return this;
+		}
+
+		public Builder title2(String title2) {
+			this.title2 = title2;
+			return this;
+		}
+
+		public Builder description(String description) {
+			this.description = description;
+			return this;
+		}
+
+		public Builder imageUrls(List<String> imageUrls) {
+			this.imageUrls = imageUrls;
+			return this;
+		}
+
+		public RentalListCard build() {
+			return new RentalListCard(this);
+		}
+	}
+
+	private RentalListCard(Builder builder) {
+		this.id = builder.id;
+		this.price = builder.price;
+		this.timeUnit = builder.timeUnit;
+		this.title1 = builder.title1;
+		this.title2 = builder.title2;
+		this.description = builder.description;
+		this.imageUrls = builder.imageUrls;
+	}
 }
