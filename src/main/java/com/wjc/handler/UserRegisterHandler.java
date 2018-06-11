611f897dd075e4253e6ad21d421b97c1a0fc1c3f
Jiechao Wang <jiechao@starmobileinc.com>
2018-06-10 22:41:38 -0400

    Check email and username in /userregister

--- src/main/java/com/wjc/handler/UserRegisterHandler.java
+++ src/main/java/com/wjc/handler/UserRegisterHandler.java
@@ -1,20 +1,60 @@
 package com.wjc.handler;
 
+import java.util.Iterator;
+
 import org.mindrot.jbcrypt.BCrypt;
 
+import com.amazonaws.services.dynamodbv2.document.Index;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
+import com.amazonaws.services.dynamodbv2.document.QueryOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.wjc.request.UserRegisterRequest;
 import com.wjc.response.UserRegisterResponse;
+import com.wjc.util.DynamoDBUtil;
 
 public class UserRegisterHandler implements RequestHandler<UserRegisterRequest, UserRegisterResponse> {
 
 	@Override
 	public UserRegisterResponse handleRequest(UserRegisterRequest request, Context context) {
+		int code = 201;
+		String message = "Register succeeded";
+		Table table = DynamoDBUtil.getDynamoDBTable("Users");
+		
+		Index index = table.getIndex("Email-index");
+		QuerySpec spec = new QuerySpec()
+			    .withKeyConditionExpression("Email = :v_email")
+			    .withValueMap(new ValueMap()
+			        .withString(":v_email", request.getEmail()));
+		ItemCollection<QueryOutcome> items = index.query(spec);
+		Iterator<Item> iter = items.iterator();
+		if (iter.hasNext()) {
+			code = 409;
+			message = "Email already exists";
+		}
+		
+		index = table.getIndex("Username-index");
+		spec = new QuerySpec()
+			    .withKeyConditionExpression("Username = :v_username")
+			    .withValueMap(new ValueMap()
+			        .withString(":v_username", request.getUsername()));
+		items = index.query(spec);
+		iter = items.iterator();
+		if (iter.hasNext()) {
+			code = 409;
+			message = "Username already exists";
+		}
+		
+		
 		UserRegisterResponse response = new UserRegisterResponse();
-		response.setStatusCode(201);
-		response.setMessage("email is " + request.getEmail() + " wechat id is " + request.getWeChatId()
-				+ " hased pw is " + hashPassword(request.getPassword()));
+		response.setStatusCode(code);
+		response.setMessage(message);
+//		response.setMessage("email is " + request.getEmail() + " wechat id is " + request.getWeChatId()
+//				+ " hased pw is " + hashPassword(request.getPassword()));
 		return response;
 	}
 
--- src/main/java/com/wjc/request/RentalDetailRequest.java
+++ src/main/java/com/wjc/request/RentalDetailRequest.java
@@ -4,43 +4,19 @@ public final class RentalDetailRequest {
 	
 	// This is the city name, not city code.
 	private String city;
-	private String neighborhood;
-	private String title;
-	// Unix epoch timestamp in milliseconds
-	private String postTime;
+	private String id;
 	
-	public RentalDetailRequest() {}
-
 	public String getCity() {
 		return city;
 	}
-
 	public void setCity(String city) {
 		this.city = city;
 	}
-
-	public String getNeighborhood() {
-		return neighborhood;
+	public String getId() {
+		return id;
 	}
-
-	public void setNeighborhood(String neighborhood) {
-		this.neighborhood = neighborhood;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getPostTime() {
-		return postTime;
-	}
-
-	public void setPostTime(String postTime) {
-		this.postTime = postTime;
+	public void setId(String id) {
+		this.id = id;
 	}
 
 }
